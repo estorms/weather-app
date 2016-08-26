b55dd27357033d2d0a82327792a6691bf23956c3
Elizabeth Sanger <libsang@gmail.com>
2016-08-25 19:28:10 -0500

    added comments throughout code

--- js/db-interactions.js
+++ js/db-interactions.js
@@ -1,69 +1,57 @@
-let userZip;
-let userWeather;
+let userZip; //global variable for userZip declared so that it can be used to store the text input field's value and and then be passed into the function that calls openWeatherMap api and populates the DOM with results
 
-$('.submit').click(function (){
-
-    userZip = $('.zip').val()
-    console.log('userZip', userZip)
-    getWeather(userZip)
-    .then(function(data) {
-        console.log(data)
-        $('#weather_output').html(`The current weather in ${data.name} is ${data.weather[0].description}`)
-    })
-
-})
+let userWeather; //global variable for userWeather declared so that it can be used to store the data received from api call to openWeatherMap and then be passed into the function that posts the api call results to Firebase
 
 
-function getWeather(zipCode) {
+function getWeather(zipCode) {  //getWeather defined as taking one arg and then returning a promise that uses that arg to make specific (zip-code based) api call
  return new Promise (function (resolve, reject){
     $.ajax({
         url: `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=dfa4f45c40ccf29bb82fe347008a1fd6`
     }).done(function(data){
-        resolve(data);
+        resolve(data); //once promise is done(run successfully), the promise is resolved (resolve method called) by receipt of that data
     })
-        .fail(function(error){
+        .fail(function(error){ //if promise fails to resolve, the promise fails (fail method called) and then rejects based on the specific error
             reject(error);
         });
     });
 }
 
+$('.submit').click(function (){  //api request initiated on click
 
-
-$('.submit').click(function (){
-
-    userZip = $('.zip').val()
+    userZip = $('.zip').val() //userZip extracted from input field
     console.log('userZip', userZip)
-    getWeather(userZip)
-    .then(function(data) {
-        // console.log(data)
-        $('#weather_output').html(`The current weather in ${data.name} is ${data.weather[0].description}`)
-        userWeather = data;
-        console.log(userWeather)
-    })
-})
-
-var dog = {
-    "height": 9,
-    "weight": "hellfire"
-}
-
-$('.save').click(function (){
-
-       saveWeather(userWeather)
+    getWeather(userZip) //getWeather function is called using userZip
+    .then(function(data) { // .then called on data received from getWeather (assumes data resolve)
+        console.log(data)
+        $('#weather_output').html(`The current weather in ${data.name} is ${data.weather[0].description}`) //data received from API call used to populate the DOM
+        userWeather = data; //VERY IMPORTANT: THIS PASSES THE DATA FROM THE API CALL TO GLOBAL VARIABLE SO CAN BE RETRIEVED LATER
     });
-
-function saveWeather(userWeather) {
- return new Promise (function (resolve, reject){
+});
+function saveWeather(userWeather) { //Save weather function takes an argument of a data object
+ return new Promise (function (resolve, reject){ //returns a promise
     $.ajax({
-        url: 'https://weather-app-a75d7.firebaseio.com/.json',
+        url: 'https://weather-app-a75d7.firebaseio.com/.json', //where to submit POST request
         type: 'POST',
-        data: JSON.stringify(userWeather)
+        data: JSON.stringify(userWeather) //calls on global variable of userWeather (which had to be stringified, even though it should be coming down as a string from openWeatherMap?)
     }).done(function(data){
         resolve(data);
-    })
+    }) //if resolved, nothing happens beyond the data having been passed
     .fail(function(error){
     reject(error);
         });
     });
 }
+
+$('.save').click(function (){ //post request submitted on click, with userWeather as data to
+
+       saveWeather(userWeather)
+    });
+
+
+
+
+
+
+
+
 
